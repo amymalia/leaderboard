2860c0c0d8a99390f0098dc0406e20813cfdeeb1
Amy Takayesu <amytakayesu@gmail.com>
2016-10-07 16:08:20 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,26 @@
-console.log("Hello WOOOOORLD!!!!");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Template.leaderboard.helpers({
+    'player' : function() {
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      let playerId = this._id;
+      let selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer) {
+        return "selected";
+      }
+    }
+  });
+
+  Template.leaderboard.events({
+    'click .player': function() {
+      let playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    }
+  });
+}
+
+if(Meteor.isServer){
+}
